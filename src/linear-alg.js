22dbc456ab0473c13d4068f3db164fcdfbc14e72
Jmeas <jellyes2@gmail.com>
2017-05-28 22:47:09 -0700

    Begin work

--- src/linear-alg.js
+++ src/linear-alg.js
@@ -1,7 +1,30 @@
-const linearAlg = {
-  greet() {
-    return 'hello';
-  }
-};
+import {
+  Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh
+} from 'three';
+import OrbitControls from 'three-orbitcontrols';
 
-export default linearAlg;
+const scene = new Scene();
+
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new BoxGeometry(1, 1, 1);
+const material = new MeshBasicMaterial({color: 0xffffff});
+const cube = new Mesh(geometry, material);
+scene.add(cube);
+
+var controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.5;
+controls.enableZoom = false;
+
+function render() {
+  renderer.render(scene, camera);
+}
+
+controls.addEventListener('change', render);
+render();
